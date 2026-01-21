96cef711bf0e056543dff90394b4416ce4b64da1
AkhilPalli78667 <pallisudhaprathima@gmail.com>
2026-01-21 02:21:37 +0000

    Final

--- src/main/java/com/BrickBreak/ Paddle.java
+++ src/main/java/com/BrickBreak/ Paddle.java
@@ -3,19 +3,138 @@ package com.BrickBreak;
 import java.awt.Graphics;
 import java.awt.*;
 
-public class Paddle {
 
+//
+//  Class author:  Akhil Palli
+//  Date created: 1/ 5/ 2026
+//  General description: Defines all the methods necessary for paddle class to run.
+//  
+public class Paddle {
 
 	//your code here!
 
-
-	
 	//don't forget you need instance variables:
-	
+	private int x;
+	private int y;
+	private int height;
+	private int speed;
+	private int width=15;
 	
 	//constructor(s):
+	public Paddle (int x, int y, int height, int speed) {
+		this.x = x;
+        this.y = y;
+        this.height = height;
+        this.speed = speed;
+	}
 	
 	//methods:
+
+	//precondition: none
+	//postcondition: fetches players paddle's velocity.
+	public int getVelocity(){
+		return this.speed;
+	}
+
+	//preconditon: takes an integer value and ads it to velocity.
+	//postcondition: updates player's paddle's velocity.
+	public int addVelocity(int adds){
+		return this.speed + adds;
+	}
+
+
+
+	//precondition: None
+	//postcondition: returns a number (width) for the players paddle.
+	public int getWidth(){
+		return this.width;
+	}
+
+
+	//precondition: none 
+	//Postcondition: Returns an integer( height ) for the player paddle
+	public int getHeight(){
+		return this.height;
+	}
+
+
+
+
+	//precondition: g is a non-null value
+	//postcondition: draws the paddle on the screen
+	public void draw(Graphics g) {
+		
+		
+		g.fillRect(x, y, width, height);
+		
+	}
+
+
+
+
+	//precondition: location is a non-null value
+	//postcondition: moves the paddle in the direction of the location 
+	public void moveX(int location) {
+	 int centerX = x + height / 2;		
+	 
+	 if (centerX<location) {
+		if ((centerX+speed)>location) {
+			x+= Math.abs(location-(centerX+speed));
+		}
+		else y+=speed;
+	 }
+	 else if (centerX>location) {
+	      if((centerX-speed)<location) {
+	    	  y-=(location-(centerX-speed));
+	      }
+	      else { y-=speed;	} 
+	 }
+	}
+
+	//precondition: none
+	//postcondition: returns the paddles y
+	public int getY() {
+		  return y;
+	  }
 	
-	
+	//precondition: none
+	//postcondition: returns the paddles x
+	  public int getX() {
+		  return x;
+	  }
+
+
+	  //precondition: ball is a non-null value
+		//postcondition: returns true if the ball is touching one the paddle this is called on
+	 public boolean isTouching(Ball ball) {
+		 int rightX= x+width;
+		 int bottomY = y+height;
+		  if(x <= ball.getXpos() && ball.getXpos()<=rightX) {
+			 System.out.println("x in bounds");
+			 if(bottomY >= ball.getYpos() && ball.getYpos()>=y) {
+				 
+				 return true;
+			 }
+	 }
+		 return false;
+	 }
+
+
+	 //precondition: accepts integer value
+	 //postcondidtion:  changes x pos of paddle.
+	 public void setX(int num){
+		this.x = num; 
+	}
+
+	//precondition: Accepts an integer value
+	//postcondition: sets the velocity  of the paddle.
+	public void setVelocity(int num ){
+		this.speed = num;	
+	}
+
+	//preconditon: none
+	//postcondition: makes players paddle moveable 
+	public void move(){
+		speed = 1;
+	}
 }
--- src/main/java/com/BrickBreak/Ball.java
+++ src/main/java/com/BrickBreak/Ball.java
@@ -1,15 +1,135 @@
 package com.BrickBreak;
 import java.awt.*;
-
+//
+//  Class author: Akhil Palli
+//  Date created:  1/5/2025
+//  General description: In this class, the Ball's movements and other functions, which are necessary
+// 						 for the balls movement are created.
+//
 public class Ball {
 	//your code here!
 	
 	//don't forget you need instance variables:
-	
+	private int x;
+	private int y;
+	private double changeX;
+	private double changeY;
+	private int size;
+
 	
 	
 	//constructor(s):
-	
+
+	public Ball(int x, int y,int size) {
+        this.x = x;
+        this.y = y;
+        this.changeX = changeX;
+        this.changeY = changeY;
+        this.size = size;
+    }
+
 	//methods:
 
+
+
+	public int getSize(){
+		return this.size;
+	}
+
+
+
+
+	//precondition: g is a non-null value
+	//postcondition: makes the ball on the screen
+   public void draw(Graphics g){
+   	 
+    
+    	g.fillOval(x, y, size, size);
+
+   }
+
+   //precondition: None
+   //postcondition: moves the ball by changeX and changeY
+   public void move() {
+	   
+    x+= changeX;
+    y+= changeY;
+   }
+
+
+   //precondition: None
+ //postcondition: changes the y direction of the ball
+   public void reverseY(){
+    changeY*=-1;
+   }
+   
+   //precondition: none
+   //postcondition: changes the x direction of the ball
+   public void reverseX(){
+    changeX*=-1;
+   }
+   
+  
+ 	//precondition: None
+ 	//postcondition: returns the ball's y value
+  	public int getYpos() {
+	  	return y;
+  	}
+  
+  	//precondition: None
+  	//postcondition: returns the ball's x value
+  	public int getXpos() {
+		  return x;
+  	}
+  
+  	//precondition: y is a non-null number
+  	//postcondition: sets the y value of the ball equal to the inputed y
+  	public void setY(int y) {
+	 	 this.y=y;
+ 	 }
+  
+	//precondition: x is a non-null number
+	//postcondition: sets the x value of the ball equal to the inputed x
+  	public void setX(int x) {
+	  	this.x=x;
+  	}
+
+
+	//precondition: none
+	//postcondition: gets the changeX of the ball equal to the inputed speed
+	public double getChangeX() {
+		return changeX;
+	}
+
+	//precondition: none
+	//postcondition: gets the changeY of the ball equal to the inputed speed
+	public double getChangeY() {
+		return changeY;
+		}
+	//precondition: x is a non-null number, and when increasing or decreasing the changeX don't change by a whole number. Use smaller decimals 
+	//postcondition: sets the changeX of the ball equal to the inputed x
+	public void setChangeX(double newX) {
+		this.changeX= newX;
+	}
+	//precondition: y is a non-null number, and when increasing or decreasing the changey don't change by a whole number. Use smaller decimals 
+	//postcondition: sets the changeY of the ball equal to the inputed y
+	public void setChangeY(double newY) {
+		this.changeY=newY;
+	}	
+
+
+	//precondition: inputs an integer value
+	// postcondition: updates x velocity of ball 
+	public void setXVelocity(int nw){
+		changeX = nw; 
+	}
+	
+	//precondition: takes an input value to change Y velocity of the ball
+	public void setYVelocity(int nw){
+		changeY = nw;
+	}
+
+
+  
+	
 }
